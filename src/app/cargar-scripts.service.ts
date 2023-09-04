21b2028463b5e6d9e54d24efd07efbc8ac07644a
KevinArzapalo <55802165+KevinArzapalo@users.noreply.github.com>
2023-09-04 15:32:03 -0600

    Update cargar-scripts.service.ts

--- src/app/cargar-scripts.service.ts
+++ src/app/cargar-scripts.service.ts
@@ -10,7 +10,7 @@ export class CargarScriptsService {
   Carga (archivos:string[]){
     for(let archivo of archivos){
       let script = document.createElement("script");
-      script.src = "assets/" + archivo + ".js";
+      script.src = "../assets/" + archivo + ".js";
       let body = document.getElementsByTagName("body")[0];
       body.appendChild(script);
     }
